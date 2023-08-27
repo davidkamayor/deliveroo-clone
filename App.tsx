630480b0935233ad7f599d5c7482ac899cf11d8b
David Kamayor <davidkamayor@gmail.com>
2023-08-27 13:51:52 -0400

    install react navigation

--- App.tsx
+++ App.tsx
@@ -1,13 +1,17 @@
 import { StatusBar } from "expo-status-bar";
 import { Text, View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
 export default function App(): React.ReactElement {
   return (
-    <View>
-      <Text className="text-red-500">
-        Open up App.tsx to start working on your app!
-      </Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <View>
+        <Text className="text-red-500">
+          Open up App.tsx to start working on your app!
+        </Text>
+        <StatusBar style="auto" />
+      </View>
+    </NavigationContainer>
   );
 }
