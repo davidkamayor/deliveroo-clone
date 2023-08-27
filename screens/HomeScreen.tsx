0293960275583295104ec9bd49da322aa1634a28
David Kamayor <davidkamayor@gmail.com>
2023-08-27 18:28:10 -0400

    rename StyledView and StyledText to View and Text

--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -1,5 +1,12 @@
-import { View, Text, Image, SafeAreaView, TextInput } from "react-native";
-import { styled } from "nativewind";
+import {
+  View,
+  Text,
+  Image,
+  SafeAreaView,
+  TextInput,
+  ScrollView,
+} from "react-native";
+import { NativeWindStyleSheet } from "nativewind";
 import React, { useLayoutEffect } from "react";
 import { useNavigation } from "@react-navigation/native";
 // use "solid" instead of "outline" for solid variant
@@ -9,9 +16,17 @@ import {
   MagnifyingGlassIcon,
   AdjustmentsVerticalIcon,
 } from "react-native-heroicons/outline";
+import Categories from "../components/Categories";
 
-const StyledView = styled(View);
-const StyledText = styled(Text);
+// without this CSS does not load on web platform
+NativeWindStyleSheet.setOutput({
+  default: "native",
+});
+
+// Configured this way in Nativewind docs. Doesn't appear to be necessary.
+// https://www.nativewind.dev/
+// const StyledView = styled(View);
+// const StyledText = styled(Text);
 
 const HomeScreen = (): React.JSX.Element => {
   const navigation = useNavigation();
@@ -26,37 +41,46 @@ const HomeScreen = (): React.JSX.Element => {
   return (
     <SafeAreaView className="bg-white pt-5">
       {/* Header */}
-      <StyledView className="flex-row pb-3 items-center mx-4 space-x-2 px-4">
+      <View className="flex-row pb-3 items-center mx-4 space-x-2 px-4">
         <Image
           source={{
             uri: "https://links.papareact.com/wru",
           }}
           className="h-7 w-7 bg-gray-300 p-4 rounded-full"
         />
-        <StyledView className="flex-1">
-          <StyledText className="font-bold text-gray-400 text-xs">
-            Deliver Now!
-          </StyledText>
-          <StyledText className="font-bold text-xl">
+        <View className="flex-1">
+          <Text className="font-bold text-gray-400 text-xs">Deliver Now!</Text>
+          <Text className="font-bold text-xl">
             Current Location
             <ChevronDownIcon size={20} color="#00CCBB" />
-          </StyledText>
-        </StyledView>
+          </Text>
+        </View>
         <UserIcon size={35} color="#00CCBB"></UserIcon>
-      </StyledView>
+      </View>
 
       {/* Search */}
-      <StyledView className="flex-row space-x-2 pb-2 mx-4">
-        <StyledView className="flex-row flex-1 space-x-2 bg-gray-200 p-3">
+      <View className="flex-row space-x-2 pb-2 mx-4">
+        <View className="flex-row flex-1 space-x-2 bg-gray-200 p-3">
           <MagnifyingGlassIcon color="gray" size={20} />
           <TextInput
             placeholder="Resturants and cuisines"
             keyboardType="default"
           />
-        </StyledView>
+        </View>
 
-        <AdjustmentsVerticalIcon></AdjustmentsVerticalIcon>
-      </StyledView>
+        <AdjustmentsVerticalIcon color="#00CCBB"></AdjustmentsVerticalIcon>
+      </View>
+
+      {/* Body */}
+      <ScrollView
+        className="bg-gray-100"
+        contentContainerStyle={{
+          paddingBottom: 100,
+        }}
+      >
+        <Categories />
+        {/* Featured Rows */}
+      </ScrollView>
     </SafeAreaView>
   );
 };
